034b9b20440b9ee484d8762d61f62068e7eb9b1f
1001-1100 <32828438+1001-1100@users.noreply.github.com>
2020-04-07 19:45:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/bao', function (req, res) {
  return res.send('Adrienne Soliven is amazing <3');
 });
 app.get('/*', function (req, res) {
